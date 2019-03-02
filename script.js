e38fdc23d1e425793cabe4dde47b3fa12901e991
Dolben <34322792+WarrenDolben@users.noreply.github.com>
2019-03-02 15:53:15 -0500

    Nick's cleaned up site
    A few changes, mainly with the header for the index page. It has actually been debugged now, and a few syntactical changes made as well.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 //script to change css 10/9/18
-
+//trying to get it to switch it back and forth 2/18/19
+//committing to github for now -- will come back to this later
 
 function colorChange() {
-    "use strict";
+        "use strict";
     
-    document.getElementById("h1").style.border = ".2rem solid goldenrod";
-    document.getElementById("nav1").style.border = ".2rem solid goldenrod";
-    document.getElementById("nav2").style.border = ".2rem solid goldenrod";
-    document.getElementById("section").style.border = ".2rem solid goldenrod";
-    document.getElementById("footer").style.border = ".2rem solid goldenrod";
-}
+        document.getElementById("h1").style.border = ".2rem solid goldenrod";
+        document.getElementById("nav1").style.border = ".2rem solid goldenrod";
+        document.getElementById("nav2").style.border = ".2rem solid goldenrod";
+        document.getElementById("section").style.border = ".2rem solid goldenrod";
+        document.getElementById("footer").style.border = ".2rem solid goldenrod";
+        document.getElementById("h1").style.border = ".2rem solid darkblue";
+        document.getElementById("nav1").style.border = ".2rem solid darkblue";
+        document.getElementById("nav2").style.border = ".2rem solid darkblue";
+        document.getElementById("section").style.border = ".2rem solid darkblue";
+        document.getElementById("footer").style.border = ".2rem solid darkblue";
+    
+    }
